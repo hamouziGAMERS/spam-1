d025d3901275c98421a71f24444bd0afc994475a
hamouziGAMERS <47302859+hamouziGAMERS@users.noreply.github.com>
2019-02-27 01:44:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 let channel =     client.channels.get("550094910158798850")
 setInterval(function() {
 channel.send(`DAMN LIFE IAM PRO SUDDEN ATTACK JAPAN FUCK LIFE`);
-}, 30)
+}, 100)
 })
 
 client.login(process.env.BOT_TOKEN);
